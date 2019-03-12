882d1ca31d63489f7ed4351751f6572d2acae577
Robert A. Ranseen <robert.ranseen@centre.edu>
2019-03-12 15:40:54 -0400

    qw

--- animationgraph.js
+++ animationgraph.js
@@ -2,14 +2,69 @@ datap = d3.json("daydata.json");
 
 datap.then(function(data)
 {
-  drawGraph();
+  drawGraph(data,0);
 },
 function(err)
 {
   console.log(err);
 })
 
-var drawGraph = function()
+var drawGraph = function(data,day)
+{
+  d3.selectAll("button.n")
+    .on("click",updateGraph(data,day+1))
+    d3.selectAll("button.p")
+      .on("click",updateGraph(data,day-1))
+  var screen =
+  {
+    width:500,
+    height:500
+  };
+
+  var svg = d3.select("svg")
+              .attr("width",screen.width)
+              .attr("height",screen.height);
+
+var margins =
+{
+  top: 10,
+  bottom: 10,
+  left: 10,
+  right:10
+};
+var width = screen.width - margins.left - margins.right;
+var height = screen.height - margins.top - margins.bottom;
+
+
+var colors = d3.scaleOrdinal(d3.schemeSet3);
+
+var barLand = svg.append("g")
+                  .classed("bar",true)
+                  .attr("transform","translate("+margins.left+","+margins.top+ ")");
+
+var bars = barLand.selectAll("rect")
+                        .data(data[0].grades)
+                        .enter()
+                        .append("rect")
+                        .attr("x",function(d,i)
+                      {
+                        return i*120;
+                      })
+                       .attr("y",function(d)
+                     {
+                         return height - d.grade*3;
+                     })
+                       .attr("width",(width/4)-5)
+                       .attr("height",function(d)
+                     {
+                       return d.grade*5;
+                     })
+                      .attr("fill",function(d){return colors(d.name);})
+
+
+}
+
+var updateGraph = function(data,day)
 {
   var screen =
   {
@@ -31,13 +86,6 @@ var margins =
 var width = screen.width - margins.left - margins.right;
 var height = screen.height - margins.top - margins.bottom;
 
-var xScale = d3.scaleLinear()
-              .domain([0,100])
-              .range(0,width);
-
-var yScale = d3.scaleLinear()
-                .domain([0,100])
-                .range(height,0);
 
 var colors = d3.scaleOrdinal(d3.schemeSet3);
 
@@ -45,30 +93,22 @@ var barLand = svg.append("g")
                   .classed("bar",true)
                   .attr("transform","translate("+margins.left+","+margins.top+ ")");
 
-var students = barLand.selectAll("g")
-                        .data(data)
-                        .enter()
-                        .append("g")
-                        .attr("fill",function(d){return colors(d.name);})
+var bars = barLand.selectAll("rect")
+                        .data(data[day].grades)
+                        .append("rect")
+                        .attr("x",function(d,i)
+                      {
+                        return i*120;
+                      })
+                       .attr("y",function(d)
+                     {
+                         return height - d.grade*3;
+                     })
+                       .attr("width",(width/4)-5)
+                       .attr("height",function(d)
+                     {
+                       return d.grade*5;
+                     })
+                      .attr("fill",function(d){return colors(d.name);})
 
-students.selectAll("rect")
-        .data(function(d)
-      {
-        return d.grade;
-      });
-       .enter()
-       .append("rect")
-       .attr("x",function(d,i)
-     {
-       return xScale(i);
-     })
-      .attr("y",function(d)
-    {
-        return yScale(d);
-    })
-      .attr("width",width/4)
-      .attr("height",function(d,i)
-    {
-      return height-d.grade;
-    })
 }
